1546fb1cd227ca336a866f9f5821dbf3c1c94269
maycuatroi <socrat.nguyenanhbinh@gmail.com>
2024-05-13 02:03:21 +0700

    - add config `app_prefix` for evo router

--- evo_django_kits/django_moduler/evo_router.py
+++ evo_django_kits/django_moduler/evo_router.py
@@ -4,23 +4,22 @@ from evo_django_kits.entities.evo_logger import EvoLogger
 
 
 class EvoRouter:
-    def __init__(self):
+    def __init__(self, app_prefix=None):
+        self.app_prefix = app_prefix
         self.logger = EvoLogger()
 
     def auto_router(self, router):
         INSTALLED_APPS = django_settings.INSTALLED_APPS
+        if self.app_prefix:
+            INSTALLED_APPS = [app for app in INSTALLED_APPS if app.startswith(self.app_prefix)]
         for app in INSTALLED_APPS:
             try:
                 app_router = __import__(f"{app}.urls", fromlist=["router"]).router
                 router.registry.extend(app_router.registry)
                 print(f"Auto register {app} router")
             except ImportError:
-                self.logger.warning(
-                    f"Cannot import {app}.urls or router not found in {app}.urls"
-                )
+                self.logger.warning(f"Cannot import {app}.urls or router not found in {app}.urls")
             except AttributeError:
-                self.logger.warning(
-                    f"Cannot import {app}.urls or router not found in {app}.urls"
-                )
+                self.logger.warning(f"Cannot import {app}.urls or router not found in {app}.urls")
 
         return router
