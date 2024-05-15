83cfa8c6a69396b3fddd508d9acb9dac4ea49883
maycuatroi <socrat.nguyenanhbinh@gmail.com>
2024-05-15 16:50:48 +0700

    - Check if `urls.py` not existed skip the import

--- evo_django_kits/django_moduler/evo_router.py
+++ evo_django_kits/django_moduler/evo_router.py
@@ -1,6 +1,8 @@
 from django.conf import settings as django_settings
 
 from evo_django_kits.entities.evo_logger import EvoLogger
+import os
+import importlib.util
 
 
 class EvoRouter:
@@ -15,9 +17,12 @@ class EvoRouter:
             INSTALLED_APPS = [app for app in INSTALLED_APPS if app.startswith(self.app_prefix)]
         for app in INSTALLED_APPS:
             try:
-                app_router = __import__(f"{app}.urls", fromlist=["router"]).router
+                urls_module = importlib.import_module(f"{app}.urls")
+                app_router = urls_module.router
                 router.registry.extend(app_router.registry)
                 print(f"Auto register {app} router")
+            except ModuleNotFoundError as e:
+                self.logger.warning(f"ModuleNotFoundError: {app}.urls not found")
 
             except ImportError as e:
                 self.logger.warning(f"Cannot import {app}.urls or router not found in {app}.urls")
