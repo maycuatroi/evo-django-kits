5f867a1597b801d4582a3c92825f1f19d8c0798b
maycuatroi <socrat.nguyenanhbinh@gmail.com>
2024-05-27 17:58:29 +0700

    - update evo router

--- evo_django_kits/django_moduler/evo_router.py
+++ evo_django_kits/django_moduler/evo_router.py
@@ -1,6 +1,8 @@
 import importlib.util
 
 from django.conf import settings as django_settings
+from django.urls import include, path
+from rest_framework.routers import BaseRouter
 
 from evo_django_kits.entities.evo_logger import EvoLogger
 
@@ -9,18 +11,25 @@ class EvoRouter:
     def __init__(self, app_prefix=None):
         self.app_prefix = app_prefix
         self.logger = EvoLogger()
+        self.routers = []
 
     def auto_router(self, router):
         INSTALLED_APPS = django_settings.INSTALLED_APPS
         IS_DEBUG = django_settings.DEBUG
         if self.app_prefix:
             INSTALLED_APPS = [app for app in INSTALLED_APPS if app.startswith(self.app_prefix)]
+        self.routers.append(router)
         for app in INSTALLED_APPS:
             try:
                 urls_module = importlib.import_module(f"{app}.urls")
-                app_router = urls_module.router
-                router.registry.extend(app_router.registry)
-                print(f"Auto register {app} router")
+                attrs = dir(urls_module)
+                for attr in attrs:
+                    attr_obj = getattr(urls_module, attr)
+                    if isinstance(attr_obj, BaseRouter):
+                        app_router = urls_module.router
+                        print(f"Auto register {app} router {attr}")
+                        self.routers.append(app_router)
+
             except ModuleNotFoundError as e:
                 if e.name == f"{app}.urls":
                     self.logger.warning(f"ModuleNotFoundError: {app}.urls not found")
@@ -37,3 +46,9 @@ class EvoRouter:
                     raise e
 
         return router
+
+    def get_paths(self, base_url: str = ""):
+        paths = []
+        for router in self.routers:
+            paths.append(path(base_url, include(router.urls)))
+        return paths
