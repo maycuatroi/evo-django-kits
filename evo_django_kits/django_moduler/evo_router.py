2a5e5a2d5e0b93fde37a2dd0cda7e6d549aaa82d
maycuatroi <socrat.nguyenanhbinh@gmail.com>
2024-12-09 02:37:38 +0700

    - enhance logic

--- evo_django_kits/django_moduler/evo_router.py
+++ evo_django_kits/django_moduler/evo_router.py
@@ -12,13 +12,16 @@ class EvoRouter:
         self.app_prefix = app_prefix
         self.logger = EvoLogger()
         self.routers = []
+        self.main_router = None
 
     def auto_router(self, router):
         INSTALLED_APPS = django_settings.INSTALLED_APPS
         IS_DEBUG = django_settings.DEBUG
         if self.app_prefix:
             INSTALLED_APPS = [app for app in INSTALLED_APPS if app.startswith(self.app_prefix)]
+        self.main_router = router
         self.routers.append(router)
+
         for app in INSTALLED_APPS:
             try:
                 urls_module = importlib.import_module(f"{app}.urls")
@@ -27,8 +30,11 @@ class EvoRouter:
                     attr_obj = getattr(urls_module, attr)
                     if isinstance(attr_obj, BaseRouter):
                         app_router = urls_module.router
-                        print(f"Auto register {app} router {attr}")
-                        self.routers.append(app_router)
+                        print(f"Auto register \033[94m{app}\033[0m router {attr}")
+
+                        # Extract routes from sub-router
+                        for prefix, viewset, basename in app_router.registry:
+                            self.main_router.register(prefix=prefix, viewset=viewset, basename=basename)
 
             except ModuleNotFoundError as e:
                 if e.name == f"{app}.urls":
@@ -48,7 +54,5 @@ class EvoRouter:
         return router
 
     def get_paths(self, base_url: str = ""):
-        paths = []
-        for router in self.routers:
-            paths.append(path(base_url, include(router.urls)))
-        return paths
+        # Since we've consolidated all routes into main_router, we only need to return its paths
+        return [path(base_url, include(self.main_router.urls))]
